944dfaba450fe33b5f22934e09a5e79fb1c4e450
popkovroman <romka200901@mail.ru>
2024-04-14 11:31:10 +0700

    fix

--- Testovoe/Testovoe/Testovoe.cpp
+++ Testovoe/Testovoe/Testovoe.cpp
@@ -4,17 +4,18 @@
 void InvertBits(uint8_t *x, size_t startbit , size_t length) {
     uint8_t mask = 0b00000000;
     size_t endbit = startbit + length;
-    for (; startbit <= endbit; startbit++) {
+    for (; startbit < endbit; startbit++) {
         mask |= (1u << 8 - startbit);
-        std::cout << std::bitset<8>(mask) << std::endl;
     }
         *x ^= mask;
 }   
+
 int main() {
     uint8_t x = 0b01110001;
+    std::cout << "Start x: " << std::bitset<8>(x) << '\n';
     size_t length = 5;
     size_t startbit = 3;
     InvertBits(&x, startbit, length);
-    std::cout << std::bitset<8>(x) << '\n';
+    std::cout << "End x:   " <<std::bitset<8>(x) << '\n';
     return 0;
 }
